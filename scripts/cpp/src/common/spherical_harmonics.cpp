8f0e5df000eead7fc94a068b089abc99bd0635a3
Alessandro Sala <ale9.sala@gmail.com>
2025-08-30 16:04:04 +0200

    Fixed x^2

--- scripts/cpp/src/common/spherical_harmonics.cpp
+++ scripts/cpp/src/common/spherical_harmonics.cpp
@@ -6,7 +6,8 @@
 #include <cmath>
 
 std::complex<double> SphericalHarmonics::Y(int l, int m, double theta,
-                                           double phi) {
+                                           double phi)
+{
   using std::abs;
   assert(l >= 0);
   assert(abs(m) <= l);
@@ -25,7 +26,8 @@ std::complex<double> SphericalHarmonics::Y(int l, int m, double theta,
     return norm * legendre * phase;
 }
 
-int SphericalHarmonics::factorial(int n) {
+int SphericalHarmonics::factorial(int n)
+{
   assert(n >= 0);
   int res = 1;
   for (int i = 2; i <= n; i++)
@@ -33,17 +35,20 @@ int SphericalHarmonics::factorial(int n) {
   return res;
 }
 
-double SphericalHarmonics::associatedLegendre(int l, int m, double x) {
+double SphericalHarmonics::associatedLegendre(int l, int m, double x)
+{
   assert(m >= 0);
   assert(m <= l);
   assert(x <= 1.0);
   double pmm = 1.0;
 
-  if (m > 0) {
+  if (m > 0)
+  {
     double sx2 = sqrt(1.0 - x * x);
     double fact = 1.0;
 
-    for (int i = 1; i <= m; ++i) {
+    for (int i = 1; i <= m; ++i)
+    {
       pmm *= -fact * sx2;
       fact += 2.0;
     }
@@ -56,7 +61,8 @@ double SphericalHarmonics::associatedLegendre(int l, int m, double x) {
     return pmmp1;
 
   double pll = 0.0;
-  for (int ll = m + 1; ll <= l; ++ll) {
+  for (int ll = m + 1; ll <= l; ++ll)
+  {
     pll = ((2 * ll - 1) * x * pmmp1 - (ll + m - 1) * pmm) / (ll - m);
     pmm = pmmp1;
     pmmp1 = pll;
@@ -64,7 +70,8 @@ double SphericalHarmonics::associatedLegendre(int l, int m, double x) {
   return pll;
 }
 SphericalHarmonics::angles SphericalHarmonics::cart2spher(double x, double y,
-                                                          double z) {
+                                                          double z)
+{
   double r = std::sqrt(x * x + y * y + z * z);
   double theta = std::acos(z / r);
   double phi = std::atan2(y, x);
@@ -72,18 +79,23 @@ SphericalHarmonics::angles SphericalHarmonics::cart2spher(double x, double y,
 }
 
 std::complex<double> SphericalHarmonics::Y(int l, int m, double x, double y,
-                                           double z) {
+                                           double z)
+{
   auto a = cart2spher(x, y, z);
   return Y(l, m, a.theta, a.phi);
 }
-Eigen::VectorXcd SphericalHarmonics::Y(int l, int m) {
+Eigen::VectorXcd SphericalHarmonics::Y(int l, int m)
+{
   auto grid = Grid::getInstance();
   Eigen::VectorXcd res(grid->get_total_spatial_points());
 
 #pragma omp parallel for collapse(3)
-  for (int i = 0; i < grid->get_n(); ++i) {
-    for (int j = 0; j < grid->get_n(); ++j) {
-      for (int k = 0; k < grid->get_n(); ++k) {
+  for (int i = 0; i < grid->get_n(); ++i)
+  {
+    for (int j = 0; j < grid->get_n(); ++j)
+    {
+      for (int k = 0; k < grid->get_n(); ++k)
+      {
         int idx = grid->idxNoSpin(i, j, k);
         double x = grid->get_xs()[i];
         double y = grid->get_ys()[j];
@@ -95,36 +107,48 @@ Eigen::VectorXcd SphericalHarmonics::Y(int l, int m) {
   return res;
 }
 
-std::complex<double> SphericalHarmonics::Q(int l, int m, Eigen::VectorXd rho) {
+std::complex<double> SphericalHarmonics::Q(int l, int m, Eigen::VectorXd rho)
+{
   auto grid = Grid::getInstance();
 
   auto pos = Fields::position();
 
-  Eigen::VectorXcd func = Y(l, m).array() * rho.array() * pos.array().pow(l);
+  using Eigen::VectorXcd;
+
+  VectorXcd YLM = Y(l, m);
+
+  assert(pos.rows() == rho.rows() == YLM.rows());
+
+  Eigen::VectorXcd func = YLM.array() * (VectorXcd(rho)).array() * (VectorXcd(pos)).array().pow(l);
 
   return Operators::integral((Eigen::VectorXcd)func, *grid);
 }
 
-double SphericalHarmonics::Y20(double theta) {
+double SphericalHarmonics::Y20(double theta)
+{
   double pi = M_PI;
   return std::sqrt(5.0 / (16.0 * pi)) *
          (3.0 * std::cos(theta) * std::cos(theta) - 1.0);
 }
-double SphericalHarmonics::Y20(double x, double y, double z) {
+double SphericalHarmonics::Y20(double x, double y, double z)
+{
   return Y20(std::acos(z / std::sqrt(x * x + y * y + z * z)));
 }
 
-double SphericalHarmonics::Y22(double theta, double phi) {
+double SphericalHarmonics::Y22(double theta, double phi)
+{
   double pi = M_PI;
   using std::cos;
   using std::sin;
 
   return std::sqrt(15.0 / (32.0 * pi)) * cos(2 * phi) * sin(theta) * sin(theta);
 }
-double SphericalHarmonics::Y22(double x, double y, double z) {
+double SphericalHarmonics::Y22(double x, double y, double z)
+{
   return Y22(std::acos(z / std::sqrt(x * x + y * y + z * z)), std::atan2(y, x));
 }
-Eigen::VectorXd SphericalHarmonics::Y20Grad(double x, double y, double z) {
+Eigen::VectorXd SphericalHarmonics::Y20Grad(double x, double y, double z)
+{
   Eigen::VectorXd grad(3);
   double r2 = x * x + y * y + z * z;
   double r = std::sqrt(r2);
